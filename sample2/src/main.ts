fc9635efb1e219ebf70263c2a03aa5551b7236d4
Kenry Phyam <tuanpt1702@gmail.com>
2019-08-08 20:22:12 +0700

    .

--- sample2/src/main.ts
+++ sample2/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api2');
   await app.listen(3100);
   console.log('http://localhost:3100');
 }
